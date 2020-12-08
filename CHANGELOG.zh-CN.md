64c5f25fb78adc61ff3f24b009baa532724a9f17
sauron <yingying.zhang@yaoxinhd.com>
2020-12-08 17:58:19 +0800

    [feat]: 添加types

--- CHANGELOG.zh-CN.md
+++ CHANGELOG.zh-CN.md
@@ -11,5 +11,6 @@
   - PageForm Table条件筛选组件
   - PageTabel 页面Table组件
   - Row 页面表格式布局
+
 ##
 <i><sup>*</sup> 在网站上动态渲染任意 HTML 是非常危险的，因为容易导致 [XSS 攻击](https://en.wikipedia.org/wiki/Cross-site_scripting)。因此请在 `dangerouslyUseHTMLString` 打开的情况下，确保 `message` 的内容是可信的，**永远不要**将用户提交的内容赋值给 `message` 属性。</i>
--- types/row.d.ts
+++ types/row.d.ts
@@ -1,5 +1,23 @@
 import { YXUIComponent } from './component'
 
+interface RowItem {
+  key: string,
+  label: string,
+  span: number,
+  lineStyle?: object
+}
+
 /** Row Component */
 export declare class YXRow extends YXUIComponent {
+  /** Title of Row */
+  title: string
+
+  /** Row span */
+  span: number
+
+  /** sourceData */
+  sourceData: object
+
+  /** row */
+  row: Array<RowItem>
 }
