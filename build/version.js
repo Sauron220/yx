ce16614300935fbb575f0f70b2eeaa5926e1c73d
sauron <yingying.zhang@yaoxinhd.com>
2020-12-09 17:12:07 +0800

    [style]: 更新样式

--- build/version.js
+++ build/version.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 const version = process.env.VERSION || require('../package.json').version
-const content = {'1.0.2': '1.0'}
+const content = {
+  '1.0.2': '1.0',
+  '1.0.2-3': '1.0'
+}
 if (!content[version]) content[version] = '1.0'
 fs.writeFileSync(path.resolve(__dirname, '../examples/versions.json'), JSON.stringify(content))
